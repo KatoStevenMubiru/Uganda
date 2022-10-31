5728569e9f0b5dac334b728f7b613aef284e7a9e
Kato Steven Mubiru <kato.steven60@gmail.com>
2022-10-31 07:31:18 +0300

    Initial Commit

---
+++ Uganda/UgandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  UgandaApp.swift
+//  Uganda
+//
+//  Created by Kato Steven Mubiru on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct UgandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
